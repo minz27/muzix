ae3595eb3f3009a2a2a8de7d75b2b3058ea23d1b
minz27 <mreenav@gmail.com>
2019-10-14 11:59:31 +0530

    Added dependencies

---
+++ src/main/java/com/stackroute/domain/Track.java
@@ -0,0 +1,54 @@
+package com.stackroute.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Track {
+    @Id
+    private int trackId;
+    private String trackName;
+    private String trackComments;
+
+    public Track(int trackId, String trackName, String trackComments) {
+        this.trackId = trackId;
+        this.trackName = trackName;
+        this.trackComments = trackComments;
+    }
+
+    public Track() {
+    }
+
+    public int getTrackId() {
+        return trackId;
+    }
+
+    public void setTrackId(int trackId) {
+        this.trackId = trackId;
+    }
+
+    public String getTrackName() {
+        return trackName;
+    }
+
+    public void setTrackName(String trackName) {
+        this.trackName = trackName;
+    }
+
+    public String getTrackComments() {
+        return trackComments;
+    }
+
+    public void setTrackComments(String trackComments) {
+        this.trackComments = trackComments;
+    }
+
+    @Override
+    public String toString() {
+        return "Track{" +
+                "trackId=" + trackId +
+                ", trackName='" + trackName + '\'' +
+                ", trackComments='" + trackComments + '\'' +
+                '}';
+    }
+}
