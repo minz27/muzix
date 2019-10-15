c39b1fe12ccdf7234459bd88e9ee9e6795ac22ae
minz27 <mreenav@gmail.com>
2019-10-15 10:07:10 +0530

    Added support for h2 console

--- src/main/java/com/stackroute/domain/Track.java
+++ src/main/java/com/stackroute/domain/Track.java
@@ -1,6 +1,10 @@
 package com.stackroute.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.annotation.processing.Generated;
 import javax.persistence.Entity;
@@ -9,6 +13,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Track {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
@@ -18,56 +26,4 @@ public class Track {
     @JsonProperty("artist")
     private String artist;
     private String trackComments;
-
-    public Track(int trackId, String trackName, String trackArtist, String trackComments) {
-        this.trackId = trackId;
-        this.name = trackName;
-        this.artist = trackArtist;
-        this.trackComments = trackComments;
-    }
-
-    public Track() {
-    }
-
-    public int getTrackId() {
-        return trackId;
-    }
-
-    public void setTrackId(int trackId) {
-        this.trackId = trackId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getArtist() {
-        return artist;
-    }
-
-    public void setArtist(String artist) {
-        this.artist = artist;
-    }
-
-    public String getTrackComments() {
-        return trackComments;
-    }
-
-    public void setTrackComments(String trackComments) {
-        this.trackComments = trackComments;
-    }
-
-    @Override
-    public String toString() {
-        return "Track{" +
-                "trackId=" + trackId +
-                ", name='" + name + '\'' +
-                ", artist='" + artist + '\'' +
-                ", trackComments='" + trackComments + '\'' +
-                '}';
-    }
 }
